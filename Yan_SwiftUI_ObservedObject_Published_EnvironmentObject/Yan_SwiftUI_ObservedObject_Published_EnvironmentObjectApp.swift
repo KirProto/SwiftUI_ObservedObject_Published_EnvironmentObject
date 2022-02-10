314226519f4c906d02a2fd7c56b9f0c41e6c8468
Kir Proto Q <kir.proto@ya.ru>
2022-02-10 10:05:08 +0500

    Initial Commit

---
+++ Yan_SwiftUI_ObservedObject_Published_EnvironmentObject/Yan_SwiftUI_ObservedObject_Published_EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_ObservedObject_Published_EnvironmentObjectApp.swift
+//  Yan_SwiftUI_ObservedObject_Published_EnvironmentObject
+//
+//  Created by KIR Q on 2022.02.10.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_ObservedObject_Published_EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
